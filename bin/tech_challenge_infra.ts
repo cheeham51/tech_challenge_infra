26f5941254d44547eea021fe36f5ab591f50082c
Tony Du <cheeham51@hotmail.com>
2022-04-30 15:21:48 +1000

    Initialise TechChallengeInfraStack

--- bin/tech_challenge_infra.ts
+++ bin/tech_challenge_infra.ts
@@ -4,11 +4,8 @@ import * as cdk from 'aws-cdk-lib';
 import { TechChallengeInfraStack } from '../lib/tech_challenge_infra-stack';
 import { TechChallengePipelineStack } from '../lib/tech_challenge_pipeline_stack';
 
-
 const app = new cdk.App();
 
-// new TechChallengeInfraStack(app, 'TechChallengeInfraStack', {
-// });
+new TechChallengeInfraStack(app, 'TechChallengeInfraStack', {});
 
-new TechChallengePipelineStack(app, 'TechChallengePipelineStack', {
-});
+new TechChallengePipelineStack(app, 'TechChallengePipelineStack', {});
