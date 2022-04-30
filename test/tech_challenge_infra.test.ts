d3038757726f5a1f66ca91063e36da2613fbeeb5
Tony Du <cheeham51@hotmail.com>
2022-04-30 12:19:58 +1000

    Initial commit

---
+++ test/tech_challenge_infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TechChallengeInfra from '../lib/tech_challenge_infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tech_challenge_infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TechChallengeInfra.TechChallengeInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
