818e2d2fa9dfcd5d58fc894a167df9e885a467e8
Tony Du <cheeham51@hotmail.com>
2022-04-30 15:43:03 +1000

    Add VPC TechChallengeInfraVpc

--- lib/tech_challenge_infra-stack.ts
+++ lib/tech_challenge_infra-stack.ts
@@ -1,8 +1,12 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import ec2 = require('aws-cdk-lib/aws-ec2');
 
 export class TechChallengeInfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // Create VPC
+    new ec2.Vpc(this, 'TechChallengeInfraVpc');
   }
 }
