0c455ff79352b3e6d5abb5b1ff80c8da267e395a
Tony Du <cheeham51@hotmail.com>
2022-05-02 19:00:24 +1000

    Delete Infrastack

--- bin/tech_challenge_infra.ts
+++ bin/tech_challenge_infra.ts
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { TechChallengeInfraStack } from '../lib/tech_challenge_infra-stack';
 import { TechChallengePipelineStack } from '../lib/tech_challenge_pipeline_stack';
 
 const app = new cdk.App();
 
 const PipelineStack = new TechChallengePipelineStack(app, 'TechChallengePipelineStack', {});
-
-const InfraStack = new TechChallengeInfraStack(app, 'TechChallengeInfraStack', {});
